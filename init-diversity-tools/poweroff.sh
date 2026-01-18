11009bcf1e9af028c9382f6b137ce9f34ee2262f
Dolphin Oracle <dolphinoracle@gmail.com>
2026-01-18 09:19:23 -0500

    remove busybox fallbacks from init specific routines

--- init-diversity-tools/poweroff.sh
+++ init-diversity-tools/poweroff.sh
@@ -4,10 +4,10 @@
 
 case "$(realpath /proc/1/exe 2>/dev/null || readlink -f /proc/1/exe)" in
     /usr/lib/systemd/systemd)
-        /usr/bin/systemctl poweroff $@ && sync && sleep 3 && /usr/bin/busybox poweroff -f;
+        /usr/bin/systemctl poweroff $@ 
         ;;
     /usr/lib/sysvinit/init)
-        /usr/lib/sysvinit/init 0 && sync && sleep 3 && /usr/bin/busybox poweroff -f;
+        /usr/lib/sysvinit/init 0 
         ;;
         *)
         sync && sleep 5 && /usr/bin/busybox poweroff -f;
--- init-diversity-tools/reboot.sh
+++ init-diversity-tools/reboot.sh
@@ -4,10 +4,10 @@
 
 case "$(realpath /proc/1/exe 2>/dev/null || readlink -f /proc/1/exe)" in
     /usr/lib/systemd/systemd)
-        /usr/bin/systemctl reboot $@ && sync && sleep 3 && /usr/bin/busybox reboot -f;
+        /usr/bin/systemctl reboot $@
         ;;
     /usr/lib/sysvinit/init)
-        /usr/lib/sysvinit/init 6 && sync && sleep 3 && /usr/bin/busybox reboot -f
+        /usr/lib/sysvinit/init 6
         ;;
         *)
         sync && sleep 3 && /usr/bin/busybox reboot -f;
